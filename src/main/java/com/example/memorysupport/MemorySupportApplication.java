995541a1bd1fb08135c55204d918efb9ee9ad054
Евгений <johnnyjohn@yandex.ru>
2023-11-20 21:29:42 +0300

    прикручиваю feign, все пока плохо (((

--- src/main/java/com/example/memorysupport/MemorySupportApplication.java
+++ src/main/java/com/example/memorysupport/MemorySupportApplication.java
@@ -2,7 +2,9 @@ package com.example.memorysupport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class MemorySupportApplication {
 
